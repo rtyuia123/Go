50ace064d225d0f1453b6b2cee34d5eb26f75db3
rtyuia123 <rtyuia123a@gmail.com>
2021-09-19 08:29:26 +0900

    version 0.3

--- syntax04.go
+++ syntax04.go
@@ -6,23 +6,23 @@ import (
 	"time"
 )
 
-//소수 판정 프로그램 v 0.2
+//소수 판정 프로그램 v 0.3
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
 
-	count := 0
-	number := rand.Intn(150) + 2 // 0과 1제외, 2~151 사이의 수
+	isPrime := true
+	number := rand.Intn(150) + 2
 	fmt.Println("임의로 추출된 수 : ", number)
 
-	for i := 2; i < number; i++ { // 1초과 num 미만 까지만 loop
+	for i := 2; i < number; i++ {
 		if number%i == 0 {
-			// count++
-			count = count + 1
+			isPrime = false
+			// count = count + 1
 		}
 	}
 
-	if count == 0 {
+	if isPrime == true {
 		fmt.Println(number, "는 소수입니다.")
 	} else {
 		fmt.Println(number, "는 소수가 아닙니다.")
