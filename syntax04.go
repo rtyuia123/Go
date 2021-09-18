0dee10faeb00cfb293ad2334f483fe853364633f
rtyuia123 <rtyuia123a@gmail.com>
2021-09-19 08:28:58 +0900

    version 0.2

--- syntax04.go
+++ syntax04.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-//소수 판정 프로그램 v 0.1
+//소수 판정 프로그램 v 0.2
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
@@ -15,34 +15,16 @@ func main() {
 	number := rand.Intn(150) + 2 // 0과 1제외, 2~151 사이의 수
 	fmt.Println("임의로 추출된 수 : ", number)
 
-	for i := 1; i <= number; i++ {
+	for i := 2; i < number; i++ { // 1초과 num 미만 까지만 loop
 		if number%i == 0 {
-			count++
+			// count++
+			count = count + 1
 		}
 	}
 
-	if count == 2 {
+	if count == 0 {
 		fmt.Println(number, "는 소수입니다.")
 	} else {
 		fmt.Println(number, "는 소수가 아닙니다.")
 	}
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"math/rand"
-// 	"time" //seed 생성용 패키지
-// )
-
-// func main() {
-// 	// seed 설정
-// 	seed := time.Now().Unix()
-// 	rand.Seed(seed)
-
-// 	for i := 1; i < 6; i++ {
-// 		dice := rand.Intn(6) + 1
-// 		fmt.Println(dice)
-// 	}
-// }
